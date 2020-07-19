c0dcca4249162620da72b4d5b6ce69589e687671
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2020-07-19 16:16:12 +0800

    openwrt
    passwall

--- scripts/update_package.sh
+++ scripts/update_package.sh
@@ -22,6 +22,7 @@ svn co https://github.com/vernesong/OpenClash/branches/master/luci-app-openclash
 git clone -b master --single-branch https://github.com/frainzy1477/luci-app-clash package/new/luci-app-clash
 sed -i 's/), 5)/), 48)/g' package/new/luci-app-clash/luasrc/controller/clash.lua
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/new/luci-app-passwall
+svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-go package/new/trojan-go
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/new/luci-app-vssr
 svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk/ package/new/luci-app-jd-dailybonus
 chmod -R 755 ./
